19b20ff6198f44a0142b79d1e0559792017198e3
Lucas Duarte <lucashdp@outlook.com>
2022-05-15 18:14:35 -0300

    feat: implements tutorial edit
    Co-authored-by: Diego Costa <costaadiego1989@users.noreply.github.com>

--- src/screens/EditTutorial.jsx
+++ src/screens/EditTutorial.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import useTutorialContext, {
+  setTutorialContext,
+} from "../contexts/useTutorialContext";
+import { useNavigate, useParams } from "react-router-dom";
 
-const Tutorial = (props) => {
+const Tutorial = () => {
+  const tutorialGlobalState = useTutorialContext();
+  const navigate = useNavigate();
+  const tutorialsMock = tutorialGlobalState.tutorialsList;
+  const { tutorialId } = useParams();
+  const tutorialToEdit = tutorialsMock.filter(
+    (tutorial) => tutorial.id === tutorialId
+  )[0];
+  const [title, setTitle] = useState(tutorialToEdit.title);
+  const [description, setDescription] = useState(tutorialToEdit.description);
+
+  const update = () => {
+    let arrayEdited = tutorialsMock;
+    arrayEdited.map((tutorial) => {
+      if (tutorial.id === tutorialId) {
+        tutorial.title = title;
+        tutorial.description = description;
+      }
+      return tutorial;
+    });
+    setTutorialContext(arrayEdited);
+    navigate("/tutorials");
+  };
+
+  const deleteTutorials = () => {
+    setTutorialContext(
+      tutorialsMock.filter((tutorial) => tutorial.id !== tutorialId)
+    );
+    navigate("/tutorials");
+  };
+
   return (
     <div>
       <div className="edit-form">
@@ -13,6 +47,8 @@ const Tutorial = (props) => {
               className="form-control"
               id="title"
               name="title"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
             />
           </div>
           <div className="form-group">
@@ -22,6 +58,8 @@ const Tutorial = (props) => {
               className="form-control"
               id="description"
               name="description"
+              value={description}
+              onChange={({ target }) => setDescription(target.value)}
             />
           </div>
 
@@ -31,8 +69,14 @@ const Tutorial = (props) => {
             </label>
           </div>
         </form>
-        <button className="badge badge-danger mr-2">Delete</button>
-        <button type="submit" className="badge badge-success">
+        <button
+          type="button"
+          className="badge badge-danger mr-2"
+          onClick={deleteTutorials}
+        >
+          Delete
+        </button>
+        <button type="button" className="badge badge-success" onClick={update}>
           Update
         </button>
       </div>
